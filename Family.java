29bf732d898125990cf5498fec02480d87d46a2c
ahajiyev628 <66174474+ahajiyev628@users.noreply.github.com>
2022-07-24 18:35:04 +0400

    Update and rename Main.java to Family.java

--- Family.java
+++ Family.java
@@ -1,31 +1,34 @@
 import java.util.Scanner;
 
-public class Main {
+public class Family {
     public static void main(String[] args){
-        Pet pet = new Pet("dog", "Rock", 5, new String[]{"eat", "drink", "sleep"});
+        Pet p = new Pet("dog", "Rock", 5, new String[]{"eat", "drink", "sleep"});
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter trick level between 1-100 for the pet: ");
-        pet.setTrickLevel(sc.nextInt());
-        System.out.println(pet.toString());
-        pet.eat();
-        pet.foul();
-        pet.respond();
 
-        Human child = new Human("Micheal", "Karleone", 1977, new Human("Jane", "Karleone"), new Human("Vito", "Karleone"));
-        System.out.print("Enter IQ between 1-100 for the child: ");
-        child.setIq(sc.nextInt());
-        System.out.println(child.toString());
+        System.out.println(p.toString());
+        p.eat();
+        p.foul();
+        p.respond();
 
         Human mother = new Human("Jane", "Karleone", 1950, new Human("Allahverdi", "Hajiyev"), new Human("Allahverdi", "Hajiyev"));
         System.out.print("Enter IQ between 1-100 for the mother: ");
         mother.setIq(sc.nextInt());
+        System.out.print("Enter trick level between 1-100 for the pet: ");
         System.out.println(mother.toString());
 
         Human father = new Human("Vito", "Karleone", 1945, new Human("Allahverdi", "Hajiyev"), new Human("Allahverdi", "Hajiyev"));
         System.out.print("Enter IQ between 1-100 for the father: ");
         father.setIq(sc.nextInt());
+        System.out.print("Enter trick level between 1-100 for the pet: ");
         System.out.println(father.toString());
+
+        Human child = new Human("Micheal", "Karleone", 1977, new Human(mother.name, mother.surname), new Human(father.name, father.surname));
+        System.out.print("Enter IQ between 1-100 for the child: ");
+        child.setIq(sc.nextInt());
+        System.out.print("Enter trick level between 1-100 for the pet: ");
+        System.out.println(child.toString());
 
         child.greetPet();
         child.describePet();
